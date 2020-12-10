2d16bf55af9a2fb4234a0a9d75b16db36c2440d8
BOLIVAR RICARDO RAMIREZ LAGOS <bolivar.ramirez@pucese.edu.ec>
2020-12-10 11:49:44 -0500

    Agregar archivos de proyecto.

---
+++ CapaLogica/Contabilidad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaLogica
+{
+    class Contabilidad
+    {
+        public double getSaldos(double saldoInicial)
+        {
+            double aux = saldoInicial * 0.1;
+            return aux;
+        }
+
+
+        public string Saludos(string nombre="Juan Perez") //parametros opcionales 
+        {
+            return "Hola" + nombre + ",Bienvenido al sistema de Contabilidad!!!";
+        }
+
+    }
+
+}
+
---
+++ CapaPresentacionForm/FrmIU.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacionForm
+{
+    public partial class FrmIU : Form
+    {
+        public FrmIU()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Capa
+        }
+    }
+}
